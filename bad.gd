2670b21d445d1c1172522fc1fa2e35efd3503467
Angel Tsang <65837801+blacitea@users.noreply.github.com>
2025-01-25 14:30:50 -0800

    on collison, change to trapped enemy and start floating up

--- bad.gd
+++ bad.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var sprite = $Sprite2D
+var hit_enemy_scene = preload("res://scene/trapped_bad.tscn")
 
 func _ready():
 	collision_layer = 1
@@ -8,5 +9,11 @@ func _ready():
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("projectile"):
-		print_debug("Bubble hit me")
-		sprite.texture = load("res://sprites/traffic cop bubbled.png")
+		var hit_enemy = hit_enemy_scene.instantiate()
+		# Add offset based on sprite height difference
+		hit_enemy.global_position = global_position + Vector2(0, 16)  # Adjust 16 to match your sprites		get_parent().add_child(hit_enemy)
+		var parent = get_parent()
+		var my_index = get_index()
+		parent.add_child(hit_enemy)
+		parent.move_child(hit_enemy, my_index)
+		queue_free()
--- scripts/trapped_bad.gd
+++ scripts/trapped_bad.gd
@@ -6,40 +6,24 @@ enum State {
 	FINISHED
 }
 
-@export var horizontal_speed: float = 500.0
-@export var vertical_speed: float = 500.0
-@export var horizontal_distance: float = 200.0
-
-var current_state: State = State.MOVING_RIGHT
+var current_state: State = State.MOVING_UP
 var initial_position: Vector2
 var distance_moved: float = 0.0
+@export var vertical_speed: float = 100.0
 
 func _ready():
 	initial_position = position
 	velocity = Vector2.ZERO
 	distance_moved = 0.0
 	add_to_group("projectile")
-
-
+	
 func _physics_process(delta):
 	match current_state:
-		State.MOVING_RIGHT:
-			_move_right(delta)
 		State.MOVING_UP:
 			_move_up(delta)
 		State.FINISHED:
 			queue_free()
-
-func _move_right(delta: float) -> void:
-	if distance_moved < horizontal_distance:
-		var movement = horizontal_speed * delta
-		velocity.x = horizontal_speed
-		position.x += movement
-		distance_moved += movement
-	else:
-		current_state = State.MOVING_UP
-		velocity.x = 0
-
+			
 func _move_up(delta: float) -> void:
 	velocity.y = -vertical_speed
 	position.y += velocity.y * delta
