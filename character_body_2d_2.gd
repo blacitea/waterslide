c6753601955290c1b5e5ff83b4b00ec21bbfc889
Angel Tsang <65837801+blacitea@users.noreply.github.com>
2025-01-24 21:25:18 -0800

    add character script and camera

--- character_body_2d_2.gd
+++ character_body_2d_2.gd
@@ -1,26 +1,21 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 7000.0
 const JUMP_FORCE = -600.0
-
-# Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_started = false
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_FORCE
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_FORCE
+		if not has_started:
+			has_started = true
+	
+	if has_started:
+		velocity.x = SPEED * delta
+		
 	move_and_slide()
