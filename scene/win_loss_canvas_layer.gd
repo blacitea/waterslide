88b8bc3730cce58acbf5b076eb6d7643adc9b664
TheWiiaboo <mikejmedley@gmail.com>
2025-01-26 11:58:45 -0800

    All i did was modify the script of WinLoss & add the Goal sprite

--- scene/win_loss_canvas_layer.gd
+++ scene/win_loss_canvas_layer.gd
@@ -1,8 +1,7 @@
 extends CanvasLayer
 
 func _on_retry_pressed():
-	pass # Replace with function body
-	
+	get_tree().change_scene_to_file("res://scene/main.tscn")
 	
 func _on_quit_pressed():
-	pass # Replace with function body
+	get_tree().quit()
