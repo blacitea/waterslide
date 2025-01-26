a890feb300de5cf9096ad1ee7a71c550346636d0
Corocis <myungjaelee93@gmail.com>
2025-01-26 14:48:13 -0800

    asdf

--- scripts/win_loss_canvas_layer.gd
+++ scripts/win_loss_canvas_layer.gd
@@ -1,7 +1,17 @@
 extends CanvasLayer
 
-func _on_retry_pressed():
-	get_tree().change_scene_to_file("res://scene/main.tscn")
+
 	
-func _on_quit_pressed():
+	
+
+	
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/level1.tscn")
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
 	get_tree().quit()
+	pass # Replace with function body.
