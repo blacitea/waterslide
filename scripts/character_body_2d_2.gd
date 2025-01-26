7758e9156612826edf6e48e311b4ea227b3591c7
Angel Tsang <65837801+blacitea@users.noreply.github.com>
2025-01-25 16:23:08 -0800

    press down arrow to fall

--- scripts/character_body_2d_2.gd
+++ scripts/character_body_2d_2.gd
@@ -2,9 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 9000.0
 const JUMP_FORCE = -600.0
+const FALL_THROUGH_SPEED = 200.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_started = false
+var is_on_platform = false
+var can_fall_through = false
 
 @export var projectile_scene: PackedScene
 @export var spawn_interval: float = 2.0
@@ -39,6 +42,9 @@ func spawn_projectile() -> void:
 
 
 func _physics_process(delta):
+	if has_started:
+		velocity.x = SPEED * delta
+		
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
@@ -48,7 +54,22 @@ func _physics_process(delta):
 		if not has_started:
 			has_started = true
 	
-	if has_started:
-		velocity.x = SPEED * delta
-	
+	if Input.is_action_pressed("ui_down") :
+		position.y += 1  # Move slightly down to trigger collision exit
+		velocity.y = FALL_THROUGH_SPEED
+		
 	move_and_slide()
+	_update_platform_status()
+	
+func _update_platform_status():
+	is_on_platform = false
+	can_fall_through = false
+
+# Check collisions
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().is_in_group("platform"):
+			is_on_platform = true
+			can_fall_through = true
+		elif collision.get_collider().is_in_group("ground"):
+			is_on_platform = true
